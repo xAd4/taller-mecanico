453384e0de453bbdd8075fa2c8cccfa4c84799de
Angel <gabrielcardozo3579@gmail.com>
2025-04-23 12:07:17 -0400

    Implementar el observador TareaObserver para crear instancias relacionadas automáticamente al crear una nueva tarea.

---
+++ app/Observers/TareaObserver.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\EstadoNeumatico;
+use App\Models\Frenos;
+use App\Models\Tarea;
+use App\Models\TrenDelantero;
+use App\Models\TrenTrasero;
+
+class TareaObserver {
+    /**
+     * Al crearse una instancia de tarea, se crea automáticamente las instancias relacionadas.
+     */
+    public function created(Tarea $tarea): void {
+        $tarea->trenDelantero()->create([]);
+        $tarea->trenTrasero()->create([]);
+        $tarea->frenos()->create([]);
+        $tarea->estadoNeumaticos()->create([]);
+    }
+}
